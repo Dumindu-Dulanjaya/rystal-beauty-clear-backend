7f2040fbb0ec520a8e84a52aaa8ceb6a6604a6be
Dumindu420 <167874993+Dumindu420@users.noreply.github.com>
2025-11-25 03:07:39 +0530

    feat: implement user creation logic in saveUser function

--- routes/controllers/userController.js
+++ routes/controllers/userController.js
@@ -5,5 +5,20 @@ export function saveUser(req, res) {
 
 
     const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-    console.log(hashedPassword);
-}
+    const user  = new User({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: hashedPassword
+
+    });
+
+    user.save().then(() => {
+        res.json({ message: "User saved successfully" });
+    }).catch((error) => {
+        res.json({
+            message: "Error saving user",
+
+        })
+
+})}
