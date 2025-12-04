8a7c8ef3dd9dd3e9526f01de9e18415d898a4b47
Dumindu420 <167874993+Dumindu420@users.noreply.github.com>
2025-12-04 23:54:57 +0530

    feat: implement createProduct function with user authentication and authorization checks

---
+++ controllers/productController.js
@@ -0,0 +1,33 @@
+import Product from "../models/product.js";
+export function createProduct(req, res) {
+    if(req.user == null) {
+        res.status(403).json({
+             message: "you need to login first" 
+            });
+            return;
+        }
+
+
+    if(req.user.role !='admin') {
+        res.status(403).json({
+                message: "You are not authorized to create a product"
+            });
+            return;
+
+        }
+        const product = new Product(req.body);
+
+        product.save().then(() => {
+            res.json({ message: "Product created successfully" });
+
+        }).catch((err) => {
+            
+            console.log(err);
+            res.status(500).json({
+                message: "product not saved",
+                
+            });
+        });
+
+
+    }
